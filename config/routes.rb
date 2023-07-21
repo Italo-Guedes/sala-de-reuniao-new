fd7c88000dc69dc13e9517c2598b71c7dfae5f48
Italo-Guedes <italotreinamentos@gmail.com>
2023-07-21 18:23:53 -0300

    Refazer com Scaffold (para controller, model e views)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :espacos
+  resources :reservas
+
+  # get 'reservas/index'
+  # get 'reservas/show'
+  # get 'reservas/new'
+  # get 'reservas/edit'
+  # get 'espacos/index'
+  # get 'espacos/show'
+  # get 'espacos/new'
+  # get 'espacos/edit'
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 
